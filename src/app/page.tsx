75bdaae405327ba39aad2067887d880813c20d71
yashwanthkrishna25 <yashwanthkrishna25>
2025-10-15 21:37:48 +0530

    01:setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { cn } from "@/lib/utils";
+
+const Page= ( ) => {
+  const something = true;
+  return (
+  <div className={cn("text-red-500 font-extrabold", )}>
+    Home Page
+    </div>
+  );
+};
+export default Page;
