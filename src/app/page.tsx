17bae879d075531f6e0a9fc9a5002b9d6d8a10a0
yashwanthkrishna25 <yashwanthkrishna25>
2025-10-16 07:46:07 +0530

    02:database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import { cn } from "@/lib/utils";
+// import { cn } from "@/lib/utils";
+// "use client";
+// import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
+// import { useEffect } from "react";
 
-const Page= ( ) => {
-  const something = true;
+const Page= async ( ) => {
+  // useEffect(() => {}, []);
+  const users = await prisma.user.findMany();
   return (
-  <div className={cn("text-red-500 font-extrabold", )}>
-    Home Page
+  <div className="min-h-screen min-w-screen flex items-center justify-center">
+   {JSON.stringify(users)}
     </div>
   );
 };
